119423e00d2b784681ce5f0770f55d3d37cd58a8
Caleb Martin <cmartin@moderncaliber.com>
2021-01-09 23:34:36 -0500

    Initial commit

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../css/App.css'
+
+function App() {
+    return (
+      <div>
+        <h1>Hello React!</h1>
+        <img src="/public/images/react.png" /> 
+      </div>
+    )
+}
+
+export default App;
